5f896a87ee7757cf1d3c0e120046fb3162c0fcfd
Morten Jagd Christensen <mortenjc@jcaps.com>
2022-10-18 17:24:37 +0200

    added tof plot for loki, changed ip addresses to office (walldisplay) server

--- scripts/loki.bash
+++ scripts/loki.bash
@@ -1,3 +1,4 @@
 ../build/bin/daqlite -f ../configs/loki/loki.json &
-../build/bin/daqlite -f ../configs/loki/lokimon_ch0.json &
-../build/bin/daqlite -f ../configs/loki/lokimon_ch1.json
+../build/bin/daqlite -f ../configs/loki/lokitof.json &
+#../build/bin/daqlite -f ../configs/loki/lokimon_ch0.json &
+#../build/bin/daqlite -f ../configs/loki/lokimon_ch1.json
