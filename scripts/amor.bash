84785e250a34ae582609e94c60b34cdb3be8fb85
Morten Jagd Christensen <mortenjc@jcaps.com>
2023-09-29 13:17:37 +0200

    add 2dtof to amor scripts

--- scripts/amor.bash
+++ scripts/amor.bash
@@ -4,4 +4,5 @@ BROKER=${DAQLITE_BROKER:-}
 
 ../build/bin/daqlite $BROKER -f ../configs/amor/amor.json &
 ../build/bin/daqlite $BROKER -f ../configs/amor/amortof.json &
-../build/bin/daqlite $BROKER -f ../configs/amor/amormon_ch0.json
+../build/bin/daqlite $BROKER -f ../configs/amor/amormon_ch0.json &
+../build/bin/daqlite $BROKER -f ../configs/amor/amortof2d.json
